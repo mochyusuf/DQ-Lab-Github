97d4007126d8bb53cc3e20bbfabfa2a0805873ca
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-13 16:52:30 +0700

    Melakukan Import File TSV - Praktikum

---
+++ R/R for Data Professional - Part 3/Melakukan Import File TSV - Praktikum.r
@@ -0,0 +1,5 @@
+#Gunakan function read.csv untuk import customer_segmentation.tsv
+data <- read.csv("https://storage.googleapis.com/dqlab-dataset/customer_segments.txt", sep="\t")
+
+#Tampilkan isi dari variable data
+data
