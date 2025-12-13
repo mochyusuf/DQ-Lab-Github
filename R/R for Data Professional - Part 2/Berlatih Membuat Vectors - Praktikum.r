e5fa89c291e3aee19c794ddb473370ff957c7887
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-13 16:37:42 +0700

    Berlatih Membuat Vectors - Praktikum

---
+++ R/R for Data Professional - Part 2/Berlatih Membuat Vectors - Praktikum.r
@@ -0,0 +1,8 @@
+#buat vector bernama my_numeric yang berisi angka 1 - 10
+my_numeric <- c(1,2,3,4,5,6,7,8,9,10)
+
+#buat vector bernama my_color yang berisi 'merah','kuning','hijau','biru','ungu'
+my_color <-c('merah','kuning','hijau','biru','ungu')
+
+#buat vector bernama my_logical yang berisi FALSE FALSE TRUE FALSE secara berurutan
+my_logical <- c(FALSE, FALSE, TRUE, FALSE)
