ee1f7cc3feeee364c11f0b18e04a2ab7eea2523a
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-13 17:11:58 +0700

    Data Eksplorasi: Kunjungan ke Dokter Gigi vs Perilaku Konsumsi

---
+++ R/Mengenal Model Regresi Linear dengan R/Data Eksplorasi Kunjungan ke Dokter Gigi vs Perilaku Konsumsi.r
@@ -0,0 +1,4 @@
+summary(data_gabungan$tingkat.kunjungan.ke.dokter.gigi)
+summary(data_gabungan$penjualan.permen)
+summary(data_gabungan$penjualan.sereal)
+summary(data_gabungan$penjualan.buah.pisang)
