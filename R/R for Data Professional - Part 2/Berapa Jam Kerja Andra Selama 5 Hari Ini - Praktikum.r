e2812e4e125981669d8d09ab34ee201f9b390818
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-13 16:38:23 +0700

    Berapa Jam Kerja Andra Selama 5 Hari Ini? - Praktikum

---
+++ R/R for Data Professional - Part 2/Berapa Jam Kerja Andra Selama 5 Hari Ini - Praktikum.r
@@ -0,0 +1,5 @@
+#Masukkan nama hari dari Senin - Jumat
+nama_hari <- c("Senin", "Selasa", "Rabu", "Kamis", "Jumat")
+
+#Masukkan jam kerja berurutan dari jam kerja di hari senin
+jam_kerja <- c(8, 7.5, 10, 7, 7.5)
