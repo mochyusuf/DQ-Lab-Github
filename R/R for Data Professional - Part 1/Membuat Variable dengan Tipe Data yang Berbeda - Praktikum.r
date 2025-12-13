7ed68e68137385011ddf1b4a3dc37864d39f868a
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-13 16:32:33 +0700

    Membuat Variable dengan Tipe Data yang Berbeda - Praktikum

---
+++ R/R for Data Professional - Part 1/Membuat Variable dengan Tipe Data yang Berbeda - Praktikum.r
@@ -0,0 +1,11 @@
+#buat variable data_numerik_1 yang berisi 7
+data_numerik_1 <- 7
+
+#buat variable data_numerik_2 yang berisi 77.7
+data_numerik_2 <- 77.7
+
+#buat variable data_character yang berisi "Belajar Data Science Sangat Menyenangkan!"
+data_character <- "Belajar Data Science Sangat Menyenangkan!"
+
+#buat variable data_logical yang berisi TRUE
+data_logical <- TRUE
