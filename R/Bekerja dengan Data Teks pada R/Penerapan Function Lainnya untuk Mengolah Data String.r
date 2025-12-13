b2daa012cc25425f5adfeccef300222190eb7eea
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-13 17:00:31 +0700

    Penerapan Function Lainnya untuk Mengolah Data String

---
+++ R/Bekerja dengan Data Teks pada R/Penerapan Function Lainnya untuk Mengolah Data String.r
@@ -0,0 +1,13 @@
+data_teks <- "Ayo belajar data di DQLab untuk memulai karir di bidang data"
+
+#penggunaan function nchar()
+nchar(data_teks)
+
+#penggunaan substr()
+substr(data_teks, 21, 25)
+
+#pengunaan sub()
+sub("data", "data science", data_teks)
+
+#pengunaan gsub()
+gsub("data", "data science", data_teks)
