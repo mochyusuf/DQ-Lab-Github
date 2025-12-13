8793a72a29a5d66ae56777ad01fc9dc0ebe3f9ba
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-13 16:32:21 +0700

    Apel ditambah Jeruk? Memang Bisa? - Praktikum

---
+++ R/R for Data Professional - Part 1/Apel ditambah Jeruk Memang Bisa - Praktikum.r
@@ -0,0 +1,9 @@
+#Kamu memiliki 5 apel dan 7 jeruk
+apel <- 5
+jeruk <- 7
+
+#Jumlahkan buah-buahan yang kamu miliki
+apel + jeruk
+
+#Simpan apel + jeruk ke dalam variable 'buah'
+buah <- apel + jeruk
