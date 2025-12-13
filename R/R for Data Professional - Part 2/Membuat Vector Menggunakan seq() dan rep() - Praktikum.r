2c50c0e912c6cbeb29f77abf81b5837828dc182f
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-13 16:38:09 +0700

    Membuat Vector Menggunakan seq() dan rep() - Praktikum

---
+++ R/R for Data Professional - Part 2/Membuat Vector Menggunakan seq() dan rep() - Praktikum.r
@@ -0,0 +1,11 @@
+#vector_seq berisi angka 1 - 2 dengan interval 0.2
+vector_seq <- seq(1, 2, 0.2)
+vector_seq
+
+#my_vector berisi "Belajar", "Data Science", "di DQLab"
+my_vector <- c("Belajar", "Data Science", "di DQLab")
+my_vector
+
+#vector_rep berisi perulangan my_vector sebanyak 3 kali
+vector_rep <- rep(my_vector, 3)
+vector_rep
