7f933a6b6d2cf2dbf08789a1a461f83db7b88065
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-13 17:11:27 +0700

    Pengaruh Konsumsi Permen Terhadap Kunjungan Rumah Sakit

---
+++ R/Mengenal Model Regresi Linear dengan R/Pengaruh Konsumsi Permen Terhadap Kunjungan Rumah Sakit.r
@@ -0,0 +1,5 @@
+penjualan_permen = read.csv("https://storage.googleapis.com/dqlab-dataset/tingkat_penjualan_kota_x_dqlab.tsv", header = TRUE, sep = "\t")
+kunjungan_dokter = read.csv("https://storage.googleapis.com/dqlab-dataset/kunjungan_dokter_gigi_kota_x_dqlab.tsv", header = TRUE, sep = "\t")
+
+print(penjualan_permen)
+print(kunjungan_dokter)
