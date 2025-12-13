f1a8ad08aae52741fa599562d059ad2a7ae51431
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-13 16:31:01 +0700

    Menggunakan Operator Matematika untuk Menyelesaikan Masalah Sederhana - Praktikum

---
+++ R/R for Data Professional - Part 1/Menggunakan Operator Matematika untuk Menyelesaikan Masalah Sederhana - Praktikum.r
@@ -0,0 +1,8 @@
+#Populasi manusia tahun depan
+7900000000 * 1.01
+
+#Populasi manusia dua tahun mendatang
+7900000000 * 1.01 * 1.01
+
+#Populasi manusia sepuluh tahun mendatang
+7900000000 * (1.01 ^ 10)
