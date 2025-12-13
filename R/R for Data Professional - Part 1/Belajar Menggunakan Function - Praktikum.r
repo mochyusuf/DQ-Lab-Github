0b41ab92ea6f6d1264098627f5900b21734e0e5a
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-13 16:33:05 +0700

    Belajar Menggunakan Function - Praktikum

---
+++ R/R for Data Professional - Part 1/Belajar Menggunakan Function - Praktikum.r
@@ -0,0 +1,17 @@
+#Hitunglah factorial dari 10
+factorial(10)
+
+#Tampilkan 5 bilangan acak yang memiliki distribusi normal
+rnorm(5, mean = 0, sd = 1)
+
+#Buatlah variabel bernama “my_variable” dan isilah dengan angka 345.678
+my_variable <- 345.678
+
+#Berapakah bilangan bulat terkecil yang lebih besar atau sama dengan my_variable?
+floor(my_variable)
+
+#Berapakah bilangan bulat terbesar yang lebih kecil atau sama dengan my_variable?
+ceiling(my_variable)
+
+#Bulatkan my_variable hingga 1 angka di belakang koma
+round(my_variable, digits = 1)
