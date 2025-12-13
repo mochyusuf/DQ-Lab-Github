4bd2b06935fa7817d76a576a7997f1fd5be092df
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-13 16:33:29 +0700

    Mini Project

---
+++ R/R for Data Professional - Part 1/Mini Project.r
@@ -0,0 +1,20 @@
+#variable hari_pahlawan
+hari_pahlawan <- as.Date("Nov 10, 2021", "%b %d, %Y")
+ 
+#menampilkan variable hari_pahlawan
+hari_pahlawan
+ 
+#variable radius
+radius <- 15
+ 
+#variable luas_lingkaran
+luas_lingkaran <- 3.14 * radius ^ 2
+ 
+#menampilkan variable luas_lingkaran
+luas_lingkaran
+ 
+#pembulatan ke atas luas_lingkaran
+ceiling(luas_lingkaran)
+ 
+#pembulatan ke bawah luas_lingkaran
+floor(luas_lingkaran)
