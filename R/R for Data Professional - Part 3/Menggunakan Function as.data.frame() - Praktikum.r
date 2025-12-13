d169bdb9f303ebd339d546b2ac59972b96307a56
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-13 16:51:15 +0700

    Menggunakan Function as.data.frame() - Praktikum

---
+++ R/R for Data Professional - Part 3/Menggunakan Function as.data.frame() - Praktikum.r
@@ -0,0 +1,8 @@
+#Buatlah sebuah vector berisi nama-nama anggota DQ Universe
+dq_universe <- c("Kroma", "Andra", "Aksara", "Antara", "Senja", "Sunyi")
+
+#Aplikasikan as.data.frame() untuk vector dq_universe
+df <- as.data.frame(dq_universe)
+
+#cek apakah variable df merupakan Dataframe
+is.data.frame(df)
