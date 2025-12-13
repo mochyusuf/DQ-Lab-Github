bf7e214a0e1e13a53704f4a3c2a72b84c945d3db
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-13 16:38:45 +0700

    Mencoba Function pada Vector - Praktikum

---
+++ R/R for Data Professional - Part 2/Mencoba Function pada Vector - Praktikum.r
@@ -0,0 +1,23 @@
+#Membuat vector berisi 7 angka dengan menggunakan function rnorm()
+my_vector <- rnorm(7, mean = 0, sd = 1)
+
+#Tampilkan isi my_vector
+my_vector
+
+#Aplikasikan function rev(x) pada my_vector
+rev(my_vector)
+
+#Aplikasikan function max(x) pada my_vector
+max(my_vector)
+
+#Aplikasikan function min(x) pada my_vector
+min(my_vector)
+
+#Aplikasikan function mean(x) pada my_vector
+mean(my_vector)
+
+#Aplikasikan function diff(x) pada my_vector
+diff(my_vector)
+
+#Aplikasikan function sum(x) pada my_vector
+sum(my_vector)
