32cd1997010b18daf577777bf9383e9ea2194391
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-13 16:32:51 +0700

    Apa ya Tipe Data dari Variabel ini? - Praktikum

---
+++ R/R for Data Professional - Part 1/Apa ya Tipe Data dari Variabel ini - Praktikum.r
@@ -0,0 +1,15 @@
+#buat variable data_numerik_1 yang berisi 7
+data_numerik_1 <- 7
+class(data_numerik_1)
+
+#buat variable data_numerik_2 yang berisi 77.7
+data_numerik_2 <- 77.7
+class(data_numerik_2)
+
+#buat variable data_character yang berisi "Belajar Data Science Sangat Menyenangkan!"
+data_character <- "Belajar Data Science Sangat Menyenangkan!"
+class(data_character)
+
+#buat variable data_logical yang berisi TRUE
+data_logical <- TRUE
+class(data_logical)
