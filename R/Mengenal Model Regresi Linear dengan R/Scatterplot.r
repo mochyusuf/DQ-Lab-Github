bf448a4b60f45e19658497caca9fddbacfcaefb9
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-13 17:12:36 +0700

    Scatterplot

---
+++ R/Mengenal Model Regresi Linear dengan R/Scatterplot.r
@@ -0,0 +1,48 @@
+plot(data_delayed_effect$kunjungan_dokter, data_delayed_effect$penjualan_permen,
+pch = 19,
+xlab = "Kunjungan dokter",
+ylab = "Penjualan Permen",
+main = "Kunjungan dokter dengan penjualan permen", 
+col = "blue")
+
+plot(data_delayed_effect$kunjungan_dokter, data_delayed_effect$penjualan_permen_1,
+pch = 19,
+xlab = "Kunjungan dokter",
+ylab = "Penjualan Permen",
+main = "Kunjungan dokter dengan penjualan permen (delay 1 bulan)", 
+col = "blue")
+
+plot(data_delayed_effect$kunjungan_dokter, data_delayed_effect$penjualan_permen_2,
+pch = 19,
+xlab = "Kunjungan dokter",
+ylab = "Penjualan Permen",
+main = "Kunjungan dokter dengan penjualan permen (delay 2 bulan)", 
+col = "blue")
+
+plot(data_delayed_effect$kunjungan_dokter, data_delayed_effect$penjualan_permen_3,
+pch = 19,
+xlab = "Kunjungan dokter",
+ylab = "Penjualan Permen",
+main = "Kunjungan dokter dengan penjualan permen (delay 3 bulan)", 
+col = "blue")
+
+plot(data_delayed_effect$kunjungan_dokter, data_delayed_effect$penjualan_permen_4,
+pch = 19,
+xlab = "Kunjungan dokter",
+ylab = "Penjualan Permen",
+main = "Kunjungan dokter dengan penjualan permen (delay 4 bulan)", 
+col = "blue")
+
+plot(data_delayed_effect$kunjungan_dokter, data_delayed_effect$penjualan_permen_5,
+pch = 19,
+xlab = "Kunjungan dokter",
+ylab = "Penjualan Permen",
+main = "Kunjungan dokter dengan penjualan permen (delay 5 bulan)", 
+col = "blue")
+
+plot(data_delayed_effect$kunjungan_dokter, data_delayed_effect$penjualan_permen_6,
+pch = 19,
+xlab = "Kunjungan dokter",
+ylab = "Penjualan Permen",
+main = "Kunjungan dokter dengan penjualan permen (delay 6 bulan)", 
+col = "blue")
