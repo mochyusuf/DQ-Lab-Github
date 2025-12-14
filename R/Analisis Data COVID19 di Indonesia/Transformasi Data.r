8244419d4011156e21555dac07aed970d0deb54f
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-14 13:36:27 +0700

    Transformasi Data

---
+++ R/Analisis Data COVID19 di Indonesia/Transformasi Data.r
@@ -0,0 +1,19 @@
+library(dplyr)
+library(tidyr)
+
+dim(cov_jabar_akumulasi)
+
+cov_jabar_akumulasi_pivot <- 
+  cov_jabar_akumulasi %>% 
+  gather(
+    key = "kategori",
+    value = "jumlah",
+    -tanggal
+  ) %>% 
+  mutate(
+    kategori = sub(pattern = "akumulasi_", replacement = "", kategori)
+  )
+
+dim(cov_jabar_akumulasi_pivot)
+
+glimpse(cov_jabar_akumulasi_pivot)
