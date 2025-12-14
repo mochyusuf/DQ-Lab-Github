7935a80b818c9d0d68154f85ce8b1878e03ddf2e
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-14 11:39:39 +0700

    Function names

---
+++ R/Data Preparation in Data Science using R/Function names.r
@@ -0,0 +1,4 @@
+# Membaca dataset csv
+penduduk.dki.csv <-read.csv("https://storage.googleapis.com/dqlab-dataset/dkikepadatankelurahan2013.csv", sep=",")
+# Menggunakan names untuk variable penduduk.dki.csv
+names(penduduk.dki.csv)
