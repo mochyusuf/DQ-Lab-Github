03bf42ebf339f5b6e17f164d454eb33cf7579d08
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-14 11:39:27 +0700

    Membaca Dataset File Excel dengan read.xlsx

---
+++ R/Data Preparation in Data Science using R/Membaca Dataset File Excel dengan read.xlsx.r
@@ -0,0 +1,4 @@
+library(openxlsx)
+# Membaca dataset dengan read.xlsx dan dimasukkan ke variable penduduk.dki
+penduduk.dki.xlsx <- read.xlsx(xlsxFile="https://storage.googleapis.com/dqlab-dataset/dkikepadatankelurahan2013.xlsx")
+str(penduduk.dki.xlsx)
