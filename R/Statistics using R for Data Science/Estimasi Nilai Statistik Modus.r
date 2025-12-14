4765b685c9892d825f5c56a25f6be5f9bd31a868
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-14 11:55:35 +0700

    Estimasi Nilai Statistik Modus

---
+++ R/Statistics using R for Data Science/Estimasi Nilai Statistik Modus.r
@@ -0,0 +1,7 @@
+library(pracma)
+
+## carilah modus untuk kolom Produk pada variable data_intro
+Mode(data_intro$Produk)
+
+## carilah modus untuk kolom Tingkat.Kepuasan pada variable data_intro
+Mode(data_intro$Tingkat.Kepuasan)
