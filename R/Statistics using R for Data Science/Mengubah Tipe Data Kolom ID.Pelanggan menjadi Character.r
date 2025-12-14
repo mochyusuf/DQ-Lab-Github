487b9ef24cc39b60a7af67edfb87517d1a450ba9
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-14 11:55:01 +0700

    Mengubah Tipe Data Kolom ID.Pelanggan menjadi Character

---
+++ R/Statistics using R for Data Science/Mengubah Tipe Data Kolom ID.Pelanggan menjadi Character.r
@@ -0,0 +1,6 @@
+## mengubah data menjadi karakter karena tidak dilakukan analisis statistik pada variabel ID Pelanggan dan nama
+data_intro$ID.Pelanggan<-as.character(data_intro$ID.Pelanggan)
+data_intro$Nama<-as.character(data_intro$Nama)
+str(data_intro$ID.Pelanggan)
+## melihat apakah sudah berhasil dalam mengubah variabel tersebut
+str(data_intro$Nama)
