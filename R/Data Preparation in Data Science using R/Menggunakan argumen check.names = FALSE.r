f2a44444258653436c6fa41071a54f14f04aef07
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-14 11:39:01 +0700

    Menggunakan argumen check.names = FALSE

---
+++ R/Data Preparation in Data Science using R/Menggunakan argumen check.names = FALSE.r
@@ -0,0 +1,4 @@
+# Membaca dataset dengan read.csv dan dimasukkan ke variable penduduk.dki
+penduduk.dki <- read.csv("https://storage.googleapis.com/dqlab-dataset/dkikepadatankelurahan2013.csv", sep=",", 
+                         check.names = FALSE)
+str(penduduk.dki)
