b69dc677568130c3471af48f5148f1be5d669d26
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-14 11:38:48 +0700

    Profile Dataset dengan Function summary

---
+++ R/Data Preparation in Data Science using R/Profile Dataset dengan Function summary.r
@@ -0,0 +1,3 @@
+# Membaca dataset dengan read.csv dan dimasukkan ke variable penduduk.dki
+penduduk.dki <- read.csv("https://storage.googleapis.com/dqlab-dataset/dkikepadatankelurahan2013.csv", sep=",")
+summary(penduduk.dki)
