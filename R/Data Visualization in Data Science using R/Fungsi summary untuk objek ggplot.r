6476423936f0a3edb11eb9b18b8ed19c53b20b54
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-14 11:16:33 +0700

    Fungsi summary untuk objek ggplot

---
+++ R/Data Visualization in Data Science using R/Fungsi summary untuk objek ggplot.r
@@ -0,0 +1,5 @@
+library(ggplot2)
+plot.jakarta <- ggplot()
+plot.jakarta <- plot.jakarta + labs(title="Luas Wilayah vs Kepadatan Penduduk DKI Jakarta")
+plot.jakarta <- plot.jakarta + labs(x = "Luas Wilayah (km2)", y="Kepadatan Jiwa per km2")
+summary(plot.jakarta)
