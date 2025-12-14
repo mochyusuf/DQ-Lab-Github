1b27ca5c3593884dfb92f71c331907628892c7f6
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-14 11:41:10 +0700

    Menambahkan kolom Penjumlahan

---
+++ R/Data Preparation in Data Science using R/Menambahkan kolom Penjumlahan.r
@@ -0,0 +1,5 @@
+library(openxlsx)
+penduduk.dki.xlsx <- read.xlsx(xlsxFile="https://storage.googleapis.com/dqlab-dataset/dkikepadatankelurahan2013.xlsx")
+# Tampilkan nama-nama kolom yang mengandung kata "perempuan".
+pola_nama_perempuan <- grep(pattern="perempuan", x = names(penduduk.dki.xlsx), ignore.case=TRUE)
+penduduk.dki.xlsx$PEREMPUAN35TAHUNKEATAS  <- rowSums(penduduk.dki.xlsx[pola_nama_perempuan])
