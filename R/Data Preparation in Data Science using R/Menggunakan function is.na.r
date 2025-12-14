aaa7342f513cbabcb142b7820d41e07648000ace
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-14 11:32:07 +0700

    Menggunakan function is.na

---
+++ R/Data Preparation in Data Science using R/Menggunakan function is.na.r
@@ -0,0 +1,6 @@
+# Buat variable x yang diisi dengan nilai NA
+x <- NA
+# Pengecekan variable x dengan nilai NA menggunakan operator ==
+x == NA
+# Pengecekan variable x dengan nilai NA menggunakan function is.na
+is.na(x)
