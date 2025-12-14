362e8c1c792140f67a9b886a72faa4ca92c587bb
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-14 11:55:11 +0700

    Mengubah Sejumlah Kolom menjadi Data Kategorik (Factor)

---
+++ R/Statistics using R for Data Science/Mengubah Sejumlah Kolom menjadi Data Kategorik (Factor).r
@@ -0,0 +1,10 @@
+# Mengubah data menjadi factor untuk membedakan data kualitatif dengan menggunakan functon as.factor
+
+data_intro$Jenis.Kelamin <- as.factor(data_intro$Jenis.Kelamin)
+data_intro$Produk <- as.factor(data_intro$Produk)
+data_intro$Tingkat.Kepuasan <- as.factor(data_intro$Tingkat.Kepuasan)
+
+# Melihat apakah sudah berhasil dalam mengubah variabel tersebut dengan menggunakan function str
+str(data_intro$Jenis.Kelamin)
+str(data_intro$Produk)
+str(data_intro$Tingkat.Kepuasan)
