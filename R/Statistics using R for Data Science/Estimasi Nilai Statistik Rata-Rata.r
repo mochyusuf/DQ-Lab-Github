1decf796fd688db0ea33b225b69799989919d7ab
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-14 11:55:58 +0700

    Estimasi Nilai Statistik Rata-Rata

---
+++ R/Statistics using R for Data Science/Estimasi Nilai Statistik Rata-Rata.r
@@ -0,0 +1,11 @@
+# carilah mean untuk kolom Pendapatan pada variable data_intro
+mean(data_intro$Pendapatan)
+
+# carilah mean untuk kolom Harga pada variable data_intro
+mean(data_intro$harga)
+
+# carilah mean untuk kolom Jumlah pada variable data_intro
+mean(data_intro$Jumlah)
+
+# carilah mean untuk kolom Total pada variable data_intro
+mean(data_intro$Total)
