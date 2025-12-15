d528b5efe6c1ae6bddb99615c5839c2f9c4c06e8
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-15 19:47:00 +0700

    Read Dataset - CSV dan TSV

---
+++ Python/Data Manipulation with Pandas - Part 1/Read Dataset - CSV dan TSV.py
@@ -0,0 +1,12 @@
+import pandas as pd
+# File CSV
+df_csv = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/sample_csv.csv")
+print(df_csv.head(3)) # Menampilkan 3 data teratas
+# File TSV
+df_tsv = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/sample_tsv.tsv", sep='\t')
+print(df_tsv.head(3)) # Menampilkan 3 data teratas
+
+
+# # [Read Dataset - Excel](https://academy.dqlab.id/main/livecode/178/347/1695)
+
+# In[11]:
