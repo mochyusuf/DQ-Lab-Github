4ca1827ce1726809df690fbb3255b76b40b804f4
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-15 10:56:02 +0700

    Praktik melalui Python untuk Klasifikasi Data Survei

---
+++ Python/Jenis Data pada Statistik/Praktik melalui Python untuk Klasifikasi Data Survei.py
@@ -0,0 +1,19 @@
+# Untuk seluruh jawaban yang ditandai dengan ___,
+#   gunakan lowercase string!
+# Jika untuk jawaban nama kolom lebih dari satu, nama kolom 
+#   harus berurut dan dinyatakan menggunakan list of string
+# Gunakan tanda petik "" untuk mengetikkan jawaban yang bertipe string.
+
+# Berdasarkan pihak pengumpul data. Data mu ini jenisnya apa?
+pihak = "primer"
+print("Berdasarkan pihak pengumpul data:", pihak)
+
+# Berdasarkan sumber data. Data mu ini jenisnya apa?
+sumber = "eksternal"
+print("\nBerdasarkan sumber data:", sumber)
+
+# Nama-nama kolom: jenis_kelamin, umur_tahun, jenis_pekerjaan,
+#   cek_medis, tinggi_badan_cm, berat_badan_kg
+# Atribut/kolom dengan data kuantitatif?
+data_kuantitatif = ["umur_tahun", "tinggi_badan_cm", "berat_badan_kg"]
+print("\nAtribut/kolom dengan data kuantitatif:\n ", data_kuantitatif)
