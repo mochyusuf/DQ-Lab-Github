3ce0cea5857971fda4bd2c0cb80bf5b65bc02434
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-15 10:56:18 +0700

    Skala Pengukuran Data: Praktik dengan Data Survei

---
+++ Python/Jenis Data pada Statistik/Skala Pengukuran Data Praktik dengan Data Survei.py
@@ -0,0 +1,23 @@
+# Untuk seluruh jawaban yang ditandai dengan ___,
+#   gunakan lowercase string!
+# Jika untuk jawaban nama kolom lebih dari satu, nama kolom 
+#   harus berurut dan dinyatakan menggunakan list of string
+# Nama-nama kolom: jenis_kelamin, umur_tahun, jenis_pekerjaan,
+#   cek_medis, tinggi_badan_cm, berat_badan_kg
+# Gunakan tanda petik "" untuk mengetikkan jawaban yang bertipe string.
+
+# Skala nominal/kategorikal
+nominal = ['jenis_kelamin','jenis_pekerjaan']
+print("Kolom dengan skala nominal/kategorikal:\n ", nominal)
+
+# Skala ordinal
+ordinal = 'cek_medis'
+print("\nKolom dengan skala ordinal:\n ", ordinal)
+
+# Skala interval
+interval = None
+print("\nKolom dengan skala interval:\n ", interval)
+
+# Skala rasio
+rasio = ['umur_tahun','tinggi_badan_cm','berat_badan_kg']
+print("\nKolom dengan skala rasio:\n ", rasio)
