eeb1aa2cee940f52bbfdd7738b322984201565f6
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-15 07:58:17 +0700

    Tugas Praktek 1

---
+++ Python/Python for Data Professional Beginner - Part 1/Tugas Praktek 1.py
@@ -0,0 +1,3 @@
+sepatu = {"nama": 'Sepatu Niko', 'Harga': 150000, "diskon": 30000 }
+baju = {"nama": 'Baju Unikloh', 'Harga': 80000, "diskon": 8000 }
+celana = {"nama": 'Celana Lepis', 'Harga': 200000, "diskon": 60000 }
