116b2e90b9679df84690382b6c4de3bf09cd13dd
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-16 09:35:23 +0700

    Tahap Modelling dengan Algoritma Machine Learning - Naive Bayes - Part 1

---
+++ Python/Pengantar Machine Learning dengan Python/Tahap Modelling dengan Algoritma Machine Learning - Naive Bayes - Part 1.py
@@ -0,0 +1,45 @@
+#Mengimport library Pandas
+import pandas as pd
+pd.set_option("display.max_column", 10)
+#Membaca dataset
+dataset_credit_scoring = pd.read_excel('https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx')
+#Membuat dataset
+dataset = dataset_credit_scoring[['pendapatan_setahun_juta', 'kpr_aktif', 'durasi_pinjaman_bulan', 'jumlah_tanggungan', 'rata_rata_overdue', 'risk_rating']]
+#Mengubah data kpr_aktif menjadi tipe integer: 'YA' = 1 dan 'TIDAK' = 0
+dataset['kpr_aktif'] = dataset['kpr_aktif'].replace(['YA', 'TIDAK'], [1, 0])
+#Mengubah data rata_rata_overdue menjadi numerik
+mapping_dict = {
+	"rata_rata_overdue": {
+		"46 - 60 days": 60,
+		"0 - 30 days": 30,
+		"31 - 45 days": 45,
+		"61 - 90 days": 90,
+		"> 90 days": 91
+	}
+}
+dataset = dataset.replace(mapping_dict)
+#Menghapus kolom pendapatan_setahun_juta dan durasi_pinjaman_bulan
+dataset = dataset.drop(['pendapatan_setahun_juta','durasi_pinjaman_bulan'], axis=1)
+
+#Import library Naive Bayes
+from sklearn.naive_bayes import GaussianNB
+
+#Input variabel x dengan drop kolom risk_rating, karena kolom risk_rating digunakan sebagai lavel (y) (dependen)
+x = dataset.drop('risk_rating',1)
+
+#Input variabel y dengan drop kolom label
+y = dataset['risk_rating'].astype('category')
+
+#Membagi data training dan data testing
+from sklearn.model_selection import train_test_split
+
+#splitting data train 80% test 20%
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+#Menjalankan data training dengan naive bayes
+model_nb = GaussianNB()
+model_nb.fit(x_train,y_train)
+
+#Menjalankan data testing
+y_predict = model_nb.predict(x_test)
+print(y_predict)
