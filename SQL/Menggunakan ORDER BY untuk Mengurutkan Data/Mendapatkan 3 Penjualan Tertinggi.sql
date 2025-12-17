5c8f51b38e61c5783d7c154d0a50c779974e2850
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-17 19:20:09 +0700

    Mendapatkan 3 Penjualan Tertinggi

---
+++ SQL/Menggunakan ORDER BY untuk Mengurutkan Data/Mendapatkan 3 Penjualan Tertinggi.sql
@@ -0,0 +1,14 @@
+SELECT 
+	EXTRACT(YEAR_MONTH from (from_unixtime(Last_Transaction/1000))) year_month_transaction,
+   	sum(Count_Transaction) total_pembelian_produk
+FROM
+	data_retail
+WHERE 
+	Product = 'Sepatu' AND
+    EXTRACT(YEAR from (from_unixtime(Last_Transaction/1000))) in (2018, 2019)
+GROUP BY
+	1
+ORDER BY 
+	2 DESC
+LIMIT 
+	3
