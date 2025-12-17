ec8f2db067413258434096698f31ee8a876d2519
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-17 18:15:13 +0700

    Apakah Pekan ini Lebih Baik?

---
+++ Python/Eksplorasi dan Analisis Data COVID-19 Indonesia using Python/Apakah Pekan ini Lebih Baik.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import requests
+resp_jabar = requests.get('https://storage.googleapis.com/dqlab-dataset/prov_detail_JAWA_BARAT.json', verify=False)
+cov_jabar_raw = resp_jabar.json()
+cov_jabar = pd.DataFrame(cov_jabar_raw['list_perkembangan'])
+
+cov_jabar_tidy = (cov_jabar.drop(columns=[item for item in cov_jabar.columns 
+                                               if item.startswith('AKUMULASI') 
+                                                  or item.startswith('DIRAWAT')])
+                           .rename(columns=str.lower)
+                           .rename(columns={'kasus': 'kasus_baru'})
+                  )
+cov_jabar_tidy['tanggal'] = pd.to_datetime(cov_jabar_tidy['tanggal']*1e6, unit='ns')
+
+cov_jabar_pekanan = (cov_jabar_tidy.set_index('tanggal')['kasus_baru']
+                                   .resample('W')
+                                   .sum()
+                                   .reset_index()
+                                   .rename(columns={'kasus_baru': 'jumlah'})
+                    )
+cov_jabar_pekanan['tahun'] = cov_jabar_pekanan['tanggal'].apply(lambda x: x.year)
+cov_jabar_pekanan['pekan_ke'] =cov_jabar_pekanan['tanggal'].apply(lambda x: x.weekofyear)
+cov_jabar_pekanan = cov_jabar_pekanan[['tahun', 'pekan_ke', 'jumlah']]
+
+print('Info cov_jabar_pekanan:')
+print(cov_jabar_pekanan.info())
+print('\nLima data teratas cov_jabar_pekanan:\n', cov_jabar_pekanan.head())
