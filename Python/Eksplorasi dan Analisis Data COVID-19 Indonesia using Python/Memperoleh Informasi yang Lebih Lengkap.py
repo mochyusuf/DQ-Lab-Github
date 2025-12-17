95a53ccfb50323f3113edb93783b11d6c1abb6c9
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-17 18:12:15 +0700

    Memperoleh Informasi yang Lebih Lengkap

---
+++ Python/Eksplorasi dan Analisis Data COVID-19 Indonesia using Python/Memperoleh Informasi yang Lebih Lengkap.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import requests
+resp_jabar = requests.get('https://storage.googleapis.com/dqlab-dataset/prov_detail_JAWA_BARAT.json', verify=False)
+cov_jabar_raw = resp_jabar.json()
+
+cov_jabar = pd.DataFrame(cov_jabar_raw['list_perkembangan'])
+print('Info cov_jabar:\n', cov_jabar.info())
+print('\nLima data teratas cov_jabar:\n', cov_jabar.head())
