2f3909bb00563ccbbf60c6b71ebb2a4cc21f5af6
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-17 19:19:34 +0700

    Penggunaan ORDER BY dengan Fungsi Bulan

---
+++ SQL/Menggunakan ORDER BY untuk Mengurutkan Data/Penggunaan ORDER BY dengan Fungsi Bulan.sql
@@ -0,0 +1,11 @@
+SELECT 
+     EXTRACT(YEAR_MONTH from (from_unixtime(Last_Transaction/1000))) month_transaction,
+     Customer_ID,
+     Product,
+     sum(Count_Transaction) total_pembelian_produk
+FROM
+     data_retail
+GROUP BY
+     1,2,3
+ORDER BY
+     1 DESC
