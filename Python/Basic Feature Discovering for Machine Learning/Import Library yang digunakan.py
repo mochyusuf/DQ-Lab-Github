4d89a9e8415e675aec8a8b0fafde646942267e31
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-17 18:52:24 +0700

    Import Library yang digunakan

---
+++ Python/Basic Feature Discovering for Machine Learning/Import Library yang digunakan.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="darkgrid")
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import StratifiedKFold
+
+import string
+import warnings
+warnings.filterwarnings('ignore', category=DeprecationWarning)
