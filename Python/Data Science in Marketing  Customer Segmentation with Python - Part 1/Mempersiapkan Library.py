db1453b07074657aa22730c0da7675ee6df0db6e
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-17 18:18:17 +0700

    Mempersiapkan Library

---
+++ Python/Data Science in Marketing  Customer Segmentation with Python - Part 1/Mempersiapkan Library.py
@@ -0,0 +1,10 @@
+import pandas as pd  
+import matplotlib.pyplot as plt  
+import seaborn as sns  
+from sklearn.preprocessing import LabelEncoder  
+  
+from kmodes.kmodes import KModes  
+from kmodes.kprototypes import KPrototypes  
+  
+import pickle  
+from pathlib import Path
