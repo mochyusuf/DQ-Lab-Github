4cf1a18ead27c02aadb6a260924f310d3191d14b
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-24 06:19:31 +0700

    Memahami fitur data

---
+++ Python/Spike SKU Feature Design/Memahami fitur data.py
@@ -0,0 +1,8 @@
+# Import library Pandas
+import pandas as pd
+
+# Baca dataset “funnel.csv”
+df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/komdigi/tbl_funnel.csv")
+
+# Tampilkan dataframe tersebut.
+df.head()
