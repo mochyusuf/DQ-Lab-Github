d40353a809d987f44fd437d113c4d08e8e81faac
Mochamad Yusuf <mochyusuf100@gmail.com>
2025-12-23 17:46:31 +0700

    Menghitung Margin

---
+++ Python/Margin and Stock Risk/Menghitung Margin.py
@@ -0,0 +1,28 @@
+#Import Library
+import pandas as pd
+
+#Load data
+df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/stock.csv')
+
+#Konversi kolom tanggal
+df['date'] = pd.to_datetime(df['date'], format='%m/%d/%Y')
+
+#Filter bulan Oktober
+df = df[df['date'].dt.month == 10]
+
+#Hitung margin terlebih dahulu
+df['margin'] = round(((df['price'] - df['cost']) / df['price']) * 100, 2)
+
+#Kelompokkan berdasarkan product_name dan hitung rata-rata margin
+df = df.groupby('product_name', as_index = False).agg({
+    'margin': 'mean',
+    'price': 'mean',
+    'cost': 'mean',
+    'stock_available': 'sum'
+})
+
+#Urutkan berdasarkan margin terbesar
+df = df.sort_values(by = 'margin', ascending = False)
+
+#Tampilkan hasil
+print(df)
